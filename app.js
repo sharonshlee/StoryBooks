e2a62b784d6d8ce961d997710c6a78efd959557e
Sharon Lee <sharonshlee@icloud.com>
2021-11-20 17:06:47 -0500

    initial setup and db connection

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const { engine } = require("express-handlebars");
+const connectDB = require("./config/db");
+
+// Load config
+dotenv.config({ path: "./config/config.env" });
+
+connectDB();
+
+const app = express();
+
+// Logging
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// Handlebars
+app.engine(".hbs", engine({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
