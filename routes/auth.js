bf8de87ec89fa4cf7bdde2cb637f4bef6e5aae1c
Sharon Lee <sharonshlee@icloud.com>
2021-11-28 18:23:32 -0500

    add authencate user, session in DB, display firstname in dashboard

--- routes/auth.js
+++ routes/auth.js
@@ -16,4 +16,11 @@ router.get(
   }
 );
 
+// @desc   Logout User
+// @route  GET /auth/logout
+router.get("/logout", (req, res) => {
+  req.logOut();
+  res.redirect("/");
+});
+
 module.exports = router;
