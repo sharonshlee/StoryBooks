aa5e958d4d9962d70de27a93f6c0453a27c79f46
Sharon Lee <sharonshlee@icloud.com>
2021-11-28 19:49:10 -0500

    add stories route

---
+++ routes/stories.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const { ensureAuth } = require("../middleware/auth");
+
+const Story = require("../models/Story");
+
+// @desc   Show add page
+// @route  GET /stories/add
+router.get("/add", ensureAuth, (req, res) => {
+  res.render("stories/add");
+});
+
+// @desc   Dashboard
+// @route  GET /dashboard
+
+module.exports = router;
