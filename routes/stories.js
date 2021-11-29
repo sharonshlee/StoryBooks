8013d870c7822bf704e3cf7ecda0f0bb17458e00
Sharon Lee <sharonshlee@icloud.com>
2021-11-29 18:38:50 -0500

    add stories

--- routes/stories.js
+++ routes/stories.js
@@ -10,7 +10,17 @@ router.get("/add", ensureAuth, (req, res) => {
   res.render("stories/add");
 });
 
-// @desc   Dashboard
-// @route  GET /dashboard
+// @desc   Process add form
+// @route  POST /stories
+router.post("/", ensureAuth, async (req, res) => {
+  try {
+    req.body.user = req.user.id;
+    await Story.create(req.body);
+    res.redirect("/dashboard");
+  } catch (err) {
+    console.error(err);
+    res.render("error/500");
+  }
+});
 
 module.exports = router;
