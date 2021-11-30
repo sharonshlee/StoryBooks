ae881d747e82dbb139fce736ad4baa88c3b0aa04
Sharon Lee <sharonshlee@icloud.com>
2021-11-30 14:11:01 -0500

    add delete story

--- routes/stories.js
+++ routes/stories.js
@@ -67,21 +67,38 @@ router.get("/edit/:id", ensureAuth, async (req, res) => {
 // @desc   Update story
 // @route  PUT /stories/:id
 router.put("/:id", ensureAuth, async (req, res) => {
-  let story = await Story.findById(req.params.id).lean();
+  try {
+    let story = await Story.findById(req.params.id).lean();
 
-  if (!story) {
-    return res.render("error/404");
+    if (!story) {
+      return res.render("error/404");
+    }
+
+    if (story.user != req.user.id) {
+      res.redirect("/stories");
+    } else {
+      story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
+        new: true,
+        runValidators: true,
+      });
+
+      res.redirect("/dashboard");
+    }
+  } catch (err) {
+    console.error(err);
+    return res.render("error/500");
   }
+});
 
-  if (story.user != req.user.id) {
-    res.redirect("/stories");
-  } else {
-    story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
-      new: true,
-      runValidators: true,
-    });
-
+// @desc   Delete story
+// @route  DELETE /stories/:id
+router.delete("/:id", ensureAuth, async (req, res) => {
+  try {
+    await Story.remove({ _id: req.params.id });
     res.redirect("/dashboard");
+  } catch (err) {
+    console.error(err);
+    return res.render("error/500");
   }
 });
 
